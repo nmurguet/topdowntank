7b8c860540c548fc93b0a31e907bea6e254d60e7
Nicolas <nefisto@gmail.com>
2017-11-07 17:24:19 -0300

    turret movement

--- toptank/Assets/scripts/TankMovement.cs
+++ toptank/Assets/scripts/TankMovement.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class TankMovement : MonoBehaviour {
 	private Rigidbody2D rb; 
 	public float moveSpeed; 
+	public float torque; 
+
+
+	//Turret
+	public GameObject turret; 
+	public float smooth; 
+	private float zVelocity = 2f;
+
+
 
 
 	// Use this for initialization
@@ -17,6 +26,7 @@ public class TankMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Movement (); 
+		MoveTurret (); 
 	}
 
 
@@ -25,8 +35,33 @@ public class TankMovement : MonoBehaviour {
 	{
 
 		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) {
-			rb.velocity = new Vector2 (rb.velocity.x, Input.GetAxisRaw ("Vertical") * moveSpeed);
+
+
+
+			rb.AddRelativeForce(Vector2.up * moveSpeed * Input.GetAxisRaw("Vertical"));
 		}
 
+
+		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) {
+
+			rb.AddTorque (Input.GetAxisRaw ("Horizontal") * -torque);
+
+
+		}
+
+	}
+
+
+
+	void MoveTurret()
+	{
+		transform.rotation = gameObject.transform.rotation; 
+
+		Vector3 diff_left = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		diff_left.Normalize ();
+
+		float rot_zl = Mathf.Atan2 (diff_left.y, diff_left.x) * Mathf.Rad2Deg;
+		float zl = Mathf.SmoothDampAngle(turret.transform.eulerAngles.z,rot_zl + 92f, ref zVelocity, smooth);
+		turret.transform.rotation = Quaternion.Euler (0f, 0f, zl);
 	}
 }
