f8951e8172e6e23810cc114e2fa95fb8155497ab
Nicolas <nefisto@gmail.com>
2017-11-07 15:12:46 -0300

    commit forward movement

---
+++ toptank/Assets/scripts/TankMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour {
+	private Rigidbody2D rb; 
+	public float moveSpeed; 
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement (); 
+	}
+
+
+
+	void Movement()
+	{
+
+		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) {
+			rb.velocity = new Vector2 (rb.velocity.x, Input.GetAxisRaw ("Vertical") * moveSpeed);
+		}
+
+	}
+}
